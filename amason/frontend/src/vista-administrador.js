c367fb13f0c076e2f2a02fba842f92aedd6f29c6
joa102 <joa102@inlumine.ual.es>
2021-03-15 13:26:48 +0100

    Cambios añadidos en vista-administrador

--- amason/frontend/src/vista-administrador.js
+++ amason/frontend/src/vista-administrador.js
@@ -1,24 +1,18 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 
-/**
- * `vista-administrador`
- *
- * VistaAdministrador element.
- *
- * @customElement
- * @polymer
- */
 class VistaAdministrador extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-horizontal-layout style="width: 100%; height: 100%;"></vaadin-horizontal-layout>
+`;
     }
 
     static get is() {
