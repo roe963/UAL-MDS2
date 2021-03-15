3350815adcf178d7dfb75ae0d6d7a6f329b058f6
joa102 <joa102@inlumine.ual.es>
2021-03-15 13:27:38 +0100

    Cambios añadidos en vista-oferta

--- amason/frontend/src/vista-oferta.js
+++ amason/frontend/src/vista-oferta.js
@@ -1,24 +1,22 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 
-/**
- * `vista-oferta`
- *
- * VistaOferta element.
- *
- * @customElement
- * @polymer
- */
 class VistaOferta extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-vertical-layout style="width: 18%; height: 26%;">
+ <label style="flex-shrink: 1; align-self: center;">Fecha</label>
+ <img style="flex-grow: 0; padding: var(--lumo-space-xl); align-self: center;">
+ <label style="flex-shrink: 1; align-self: center;">Precio</label>
+</vaadin-vertical-layout>
+`;
     }
 
     static get is() {
