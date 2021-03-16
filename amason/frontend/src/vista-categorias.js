9c60f45130211e13846560b2efd5d686157f5fb1
Juan Diego Ortega Aranda <joa102@inlumine.ual.es>
2021-03-16 12:52:34 +0100

    cambios realizados en vista-categorias

--- amason/frontend/src/vista-categorias.js
+++ amason/frontend/src/vista-categorias.js
@@ -1,24 +1,21 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import './vista-categoria.js';
 
-/**
- * `vista-categorias`
- *
- * VistaCategorias element.
- *
- * @customElement
- * @polymer
- */
 class VistaCategorias extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-horizontal-layout class="content" style="width: 100%; height: 100%;">
+ <vista-categoria style="width: 100%; height: 12%;"></vista-categoria>
+</vaadin-horizontal-layout>
+`;
     }
 
     static get is() {
