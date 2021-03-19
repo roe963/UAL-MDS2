20daab1f67e2eb13bdad5bf1d1aa42777da4e0c1
RubenOrtegaEnciso <roe963@inlumne.ual.es>
2021-03-19 13:57:56 +0100

    Navegabilidad en el MainView

--- amason/src/main/java/ual/mds2/ortegaortega/MainView.java
+++ amason/src/main/java/ual/mds2/ortegaortega/MainView.java
@@ -1,5 +1,7 @@
 package ual.mds2.ortegaortega;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -10,6 +12,7 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
+import interfaz.Carrito;
 import interfaz.Cliente;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,35 +20,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 /**
  * A sample Vaadin view class.
  * <p>
- * To implement a Vaadin view just extend any Vaadin component and
- * use @Route annotation to announce it in a URL as a Spring managed
- * bean.
- * Use the @PWA annotation make the application installable on phones,
- * tablets and some desktop browsers.
+ * To implement a Vaadin view just extend any Vaadin component and use @Route
+ * annotation to announce it in a URL as a Spring managed bean. Use the @PWA
+ * annotation make the application installable on phones, tablets and some
+ * desktop browsers.
  * <p>
- * A new instance of this class is created for every new user and every
- * browser tab/window.
+ * A new instance of this class is created for every new user and every browser
+ * tab/window.
  */
 @Route
-@PWA(name = "Vaadin Application",
-        shortName = "Vaadin App",
-        description = "This is an example Vaadin application.",
-        enableInstallPrompt = false)
+@PWA(name = "Vaadin Application", shortName = "Vaadin App", description = "This is an example Vaadin application.", enableInstallPrompt = false)
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 public class MainView extends VerticalLayout {
 
-    /**
-     * Construct a new Vaadin view.
-     * <p>
-     * Build the initial UI state for the user accessing the application.
-     *
-     * @param service The message service. Automatically injected Spring managed bean.
-     */
-    
+//	public VerticalLayout vl; 
+
+	/**
+	 * Construct a new Vaadin view.
+	 * <p>
+	 * Build the initial UI state for the user accessing the application.
+	 *
+	 * @param service The message service. Automatically injected Spring managed
+	 *                bean.
+	 */
+
 	public MainView(@Autowired GreetService service) {
 		Cliente cliente = new Cliente();
-    	add(cliente);
-    }
+		Carrito carrito = new Carrito();
+
+		add(cliente);
+
+		cliente.getVerCarrito().addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+
+			@Override
+			public void onComponentEvent(ClickEvent<Button> event) {
+				// TODO Auto-generated method stub
+				removeAll();
+				add(carrito);
+			}
+		});
+
+		carrito.getaInicio().addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+			@Override
+			public void onComponentEvent(ClickEvent<Button> event) {
+				// TODO Auto-generated method stub
+				removeAll();
+				add(cliente);
+			}
+		});
+	}
 
 }
