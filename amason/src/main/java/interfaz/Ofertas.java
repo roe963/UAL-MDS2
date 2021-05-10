1a5977c058a24fb1b5d838fc7791e2a786b9751c
Juan Diego Ortega Aranda <joa102@inlumine.ual.es>
2021-05-10 17:53:23 +0200

    subida ofertas interfaz

---
+++ amason/src/main/java/interfaz/Ofertas.java
@@ -0,0 +1,10 @@
+package interfaz;
+
+public class Ofertas {
+	/*public Cliente _cliente;
+	public Oferta_producto _ofertas;*/
+
+	public void cargar_ofertas() {
+		throw new UnsupportedOperationException();
+	}
+}
