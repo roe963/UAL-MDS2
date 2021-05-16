24785d0be68ae351cca20b358f4243066d1785d0
Juan Diego Ortega Aranda <joa102@inlumine.ual.es>
2021-05-16 12:34:20 +0200

    vista tramitar pedido

---
+++ amason/src/main/java/interfaz/Tramitar_pedido.java
@@ -0,0 +1,14 @@
+package interfaz;
+
+import vistas.VistaTramitarpedido;
+
+public class Tramitar_pedido extends VistaTramitarpedido {
+	/*private event _comprar;
+	public Carrito_cliente_registrado _carrito_cliente_registrado;
+	public Datos_de_compra _datos_de_compra;
+	public Verificar_compra _verificar_compra;*/
+
+	public void comprar() {
+		throw new UnsupportedOperationException();
+	}
+}
