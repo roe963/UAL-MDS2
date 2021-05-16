8d4d7934d1e218f670cd680e29167b464cce4ebf
Juan Diego Ortega Aranda <joa102@inlumine.ual.es>
2021-05-16 21:19:27 +0200

    ver pedidos

---
+++ amason/src/main/java/interfaz/Ver_pedidos.java
@@ -0,0 +1,12 @@
+package interfaz;
+
+import vistas.VistaVerpedidos;
+
+public class Ver_pedidos extends VistaVerpedidos {
+	/*public Cliente_registrado _cliente_registrado;
+	public Pedidos_cliente_registrado _pedidos_cliente_registrado;*/
+
+	public void cargar_pedidos_cliente_registrado() {
+		throw new UnsupportedOperationException();
+	}
+}
