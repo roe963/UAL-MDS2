db54850c51c880effa65fbfc1c955cc71ab7ee17
Juan Diego Ortega Aranda <joa102@inlumine.ual.es>
2021-05-16 21:35:24 +0200

    productos del pedido

---
+++ amason/src/main/java/interfaz/Producto_del_pedido.java
@@ -0,0 +1,14 @@
+package interfaz;
+
+import vistas.VistaProductodelpedido;
+
+public class Producto_del_pedido extends VistaProductodelpedido {
+	/*private label _nombreProducto;
+	private label _nombreCategoria;
+	private label _cantidad;
+	private label _precio;
+	private label _precioPedido;
+	private button _valorar;
+	public Productos_del_pedido _productos_del_pedido;
+	public Valorar_producto _valorar_producto;*/
+}
---
+++ amason/src/main/java/interfaz/Productos_del_pedido.java
@@ -0,0 +1,14 @@
+package interfaz;
+
+import java.util.Vector;
+import interfaz.Producto_del_pedido;
+import vistas.VistaProductosdelpedido;
+
+public class Productos_del_pedido extends VistaProductosdelpedido {
+	/*public Ver_pedido _ver_pedido;
+	public Vector<Producto_del_pedido> _producto_del_pedido = new Vector<Producto_del_pedido>();*/
+
+	public void valorar_producto() {
+		throw new UnsupportedOperationException();
+	}
+}
