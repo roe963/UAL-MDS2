65e8d164a21789d4d2446071276179d973edb96e
Juan Diego Ortega Aranda <joa102@inlumine.ual.es>
2021-05-19 17:29:47 +0200

    vista ver respuesta

---
+++ amason/src/main/java/Ver_respuestas.java
@@ -0,0 +1,11 @@
+import interfaz.Mensaje;
+import interfaz.Responder;
+import interfaz.Respuestas;
+
+public class Ver_respuestas {
+	private button _responderMensaje;
+	private button _volverAtras;
+	public Mensaje _mensaje;
+	public Responder _responder;
+	public Respuestas _respuestas;
+}
