63db4fb1db8b0f2286731263c449d7e9cb947deb
Ruben Ortega Enciso <roe963@inlumine.ual.es>
2021-06-23 18:39:32 +0200

    Login arregaldo

--- amason/src/main/java/basededatos/Usuario.java
+++ amason/src/main/java/basededatos/Usuario.java
@@ -19,74 +19,78 @@ import javax.persistence.*;
 import org.orm.PersistentException;
 import org.orm.PersistentTransaction;
 
+import com.vaadin.flow.component.UI;
+
 import interfaz.Administrador;
 import interfaz.Cliente;
 import interfaz.Empresa_transportes;
 import interfaz.Encargado_compras;
 import interfaz.Ver_catalogo_administrador;
-import ual.mds2.ortegaortega.ViewChanger;
+import ual.mds2.ortegaortega.MenuHeader;
+import ual.mds2.ortegaortega.Session;
+import ual.mds2.ortegaortega.TIPOUSUARIO;
 
 @Entity
-@org.hibernate.annotations.Proxy(lazy=false)
-@Table(name="Usuario")
-@Inheritance(strategy=InheritanceType.JOINED)
+@org.hibernate.annotations.Proxy(lazy = false)
+@Table(name = "Usuario")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Usuario implements Serializable {
-	public Usuario() {
-	}
-	
-	@Column(name="Id", nullable=false, length=10)	
-	@Id	
-	@GeneratedValue(generator="BASEDEDATOS_USUARIO_ID_GENERATOR")	
-	@org.hibernate.annotations.GenericGenerator(name="BASEDEDATOS_USUARIO_ID_GENERATOR", strategy="native")	
-	private int id;
-	
-	@Column(name="Email", nullable=true, length=255)	
-	private String email;
-	
-	@Column(name="Password", nullable=true, length=255)	
-	private String password;
-	
-	@Column(name="Activo", nullable=false, length=1)	
-	private boolean activo;
-	
-	private void setId(int value) {
-		this.id = value;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public int getORMID() {
-		return getId();
-	}
-	
-	public void setEmail(String value) {
-		this.email = value;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setPassword(String value) {
-		this.password = value;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public void setActivo(boolean value) {
-		this.activo = value;
-	}
-	
-	public boolean getActivo() {
-		return activo;
-	}
-	
-	public basededatos.Usuario iniciar_sesion(String mailUsuario, String passwordUsuario) {
-		Usuario[] usuarios = null;
+    public Usuario() {
+    }
+
+    @Column(name = "Id", nullable = false, length = 10)
+    @Id
+    @GeneratedValue(generator = "BASEDEDATOS_USUARIO_ID_GENERATOR")
+    @org.hibernate.annotations.GenericGenerator(name = "BASEDEDATOS_USUARIO_ID_GENERATOR", strategy = "native")
+    private int id;
+
+    @Column(name = "Email", nullable = true, length = 255)
+    private String email;
+
+    @Column(name = "Password", nullable = true, length = 255)
+    private String password;
+
+    @Column(name = "Activo", nullable = false, length = 1)
+    private boolean activo;
+
+    private void setId(int value) {
+        this.id = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getORMID() {
+        return getId();
+    }
+
+    public void setEmail(String value) {
+        this.email = value;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPassword(String value) {
+        this.password = value;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setActivo(boolean value) {
+        this.activo = value;
+    }
+
+    public boolean getActivo() {
+        return activo;
+    }
+
+    public basededatos.Usuario iniciar_sesion(String mailUsuario, String passwordUsuario) {
+        Usuario[] usuarios = null;
         PersistentTransaction t;
         try {
             t = basededatos.MDS12021PFOrtegaOrtegaPersistentManager.instance().getSession().beginTransaction();
@@ -105,31 +109,42 @@ public class Usuario implements Serializable {
         if (usuarios.length > 0) {
             Usuario aux = new bds.Clientes().iniciar_sesion_cliente(usuarios[0].id);
             if (aux != null) {
-                ViewChanger.usuario = ViewChanger.TIPOUSUARIO.REGISTRADO;
+//                ViewChanger.usuario = ViewChanger.TIPOUSUARIO.REGISTRADO;
+                Session.setTipo(TIPOUSUARIO.REGISTRADO);
 //                ViewChanger.SaveSession(aux.getId());
-                ViewChanger.CambiarVista(new Cliente());
+//                ViewChanger.CambiarVista(new Cliente());
+                MenuHeader.generarMenuBar();
+                UI.getCurrent().navigate("cliente");
                 return aux;
             } else {
                 aux = new bds.Administradores().iniciar_sesion_administrador(usuarios[0].id);
                 if (aux != null) {
-                    ViewChanger.usuario = ViewChanger.TIPOUSUARIO.ADMIN;
+//                    ViewChanger.usuario = ViewChanger.TIPOUSUARIO.ADMIN;
+                    Session.setTipo(TIPOUSUARIO.ADMIN);
 //                    ViewChanger.SaveSession(aux.getId());
-                    ViewChanger.CambiarVista(new Administrador());
-                    //ViewChanger.CambiarVista(new Ver_catalogo_administrador());
+//                    ViewChanger.CambiarVista(new Administrador());
+                    MenuHeader.generarMenuBar();
+                    UI.getCurrent().navigate("administrador");
                     return aux;
                 } else {
                     aux = new bds.Encargados_compras().iniciar_sesion_encargado_compras(usuarios[0].id);
                     if (aux != null) {
-                        ViewChanger.usuario = ViewChanger.TIPOUSUARIO.ENCARGADO;
+//                        ViewChanger.usuario = ViewChanger.TIPOUSUARIO.ENCARGADO;
+                        Session.setTipo(TIPOUSUARIO.ENCARGADO);
 //                        ViewChanger.SaveSession(aux.getId());
-                        ViewChanger.CambiarVista(new Encargado_compras());
+//                        ViewChanger.CambiarVista(new Encargado_compras());
+                        MenuHeader.generarMenuBar();
+                        UI.getCurrent().navigate("encargadocompras");
                         return aux;
                     } else {
                         aux = new bds.Empresas_transportes().iniciar_sesion_empresa_transportes(usuarios[0].id);
                         if (aux != null) {
-                            ViewChanger.usuario = ViewChanger.TIPOUSUARIO.TRANSPORTES;
+//                            ViewChanger.usuario = ViewChanger.TIPOUSUARIO.TRANSPORTES;
+                            Session.setTipo(TIPOUSUARIO.TRANSPORTES);
 //                            ViewChanger.SaveSession(aux.getId());
-                            ViewChanger.CambiarVista(new Empresa_transportes());
+//                            ViewChanger.CambiarVista(new Empresa_transportes());
+                            MenuHeader.generarMenuBar();
+                            UI.getCurrent().navigate("empresatransportes");
                             return aux;
                         } else {
                             return usuarios[0];
@@ -140,15 +155,15 @@ public class Usuario implements Serializable {
         } else {
             return null;
         }
-	}
-	
-	public boolean recuperar_contrasena(String mailUsuario) {
-		//TODO: Implement Method
-		throw new UnsupportedOperationException();
-	}
-	
-	public String toString() {
-		return String.valueOf(getId());
-	}
-	
+    }
+
+    public boolean recuperar_contrasena(String mailUsuario) {
+        // TODO: Implement Method
+        throw new UnsupportedOperationException();
+    }
+
+    public String toString() {
+        return String.valueOf(getId());
+    }
+
 }
