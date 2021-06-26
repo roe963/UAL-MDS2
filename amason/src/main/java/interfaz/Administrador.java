9dda7308e84efe1144e5913667dedf743f141e23
Juan Diego Ortega Aranda <joa102@inlumine.ual.es>
2021-06-26 11:28:15 +0200

    actualizar route administrador

--- amason/src/main/java/interfaz/Administrador.java
+++ amason/src/main/java/interfaz/Administrador.java
@@ -1,7 +1,12 @@
 package interfaz;
 
+import com.vaadin.flow.router.PreserveOnRefresh;
+import com.vaadin.flow.router.Route;
+
 import vistas.VistaAdministrador;
 
+@PreserveOnRefresh
+@Route("administrador")
 //public class Administrador extends VistaAdministrador {
 public class Administrador extends Ver_catalogo_administrador {
 
