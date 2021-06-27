1c6df5db3cbe2b099ecf1f0db23bdc965bf1352d
Juan Diego Ortega Aranda <joa102@inlumine.ual.es>
2021-06-27 21:48:34 +0200

    conflictos producto java

--- amason/src/main/java/interfaz/Productos.java
+++ amason/src/main/java/interfaz/Productos.java
@@ -1,35 +1,11 @@
 package interfaz;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.notification.Notification.Position;
-import com.vaadin.flow.component.html.Label;
-
-import basededatos.Producto;
+import basededatos.Categoria;
 import bds.BDPrincipal;
 import bds.iCliente;
 import vistas.VistaProductos;
 
 public class Productos extends VistaProductos {
-	//public ArrayList<Producto> _producto = new ArrayList<Producto>();
-	
-	iCliente clientes = new BDPrincipal();
-	
-	public Productos() {
-		
-		Producto[] productos = clientes.cargar_productos();	
-		
-		this.getVaadinHorizontalLayout().removeAll();
-		if (productos.length != 0) {
-            for (int i = 0; i < productos.length; i++) {
-                this.getVaadinHorizontalLayout().add(new interfaz.Producto(productos[i]));
-            }
-        }else {
-            Label titulo= new Label();
-            titulo.setText("No hay productos en la BD");
-            this.getVaadinHorizontalLayout().add(titulo);
 
     iCliente clientes = new BDPrincipal();
 
@@ -50,7 +26,5 @@ public class Productos extends VistaProductos {
         for (int i = 0; i < productos.length; i++) {
             this.getLayoutProductos().add(new Producto(productos[i]));
         }
-		
-	}
-	
+    }
 }
