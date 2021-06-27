a34245c02839a9dbe8e372035b67175aa56f305d
Juan Diego Ortega Aranda <joa102@inlumine.ual.es>
2021-06-27 21:38:44 +0200

    conflictos productos

--- amason/src/main/java/interfaz/Productos.java
+++ amason/src/main/java/interfaz/Productos.java
@@ -30,6 +30,25 @@ public class Productos extends VistaProductos {
             Label titulo= new Label();
             titulo.setText("No hay productos en la BD");
             this.getVaadinHorizontalLayout().add(titulo);
+
+    iCliente clientes = new BDPrincipal();
+
+    public Productos() {
+
+        basededatos.Producto[] productos = clientes.cargar_productos();
+        
+        this.getLayoutProductos().removeAll();
+        for (int i = 0; i < productos.length; i++) {
+            this.getLayoutProductos().add(new Producto(productos[i]));
+        }
+    }
+
+    public Productos(Categoria categoria) {
+        basededatos.Producto[] productos = clientes.cargar_productos_categoria(categoria.getId());
+        
+        this.getLayoutProductos().removeAll();
+        for (int i = 0; i < productos.length; i++) {
+            this.getLayoutProductos().add(new Producto(productos[i]));
         }
 		
 	}
